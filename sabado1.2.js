34b27da2de707c0f854ee322be2832d0c80f8f34
Marta Cezario <142020079+martacezario@users.noreply.github.com>
2024-02-26 18:54:02 +0000

    Add files via upload

---
+++ sabado1.2.js
@@ -0,0 +1,11 @@
+//escreva um programa que verifica se um numero é positivo, negativo ou zero.
+
+const numero = 8
+
+if(numero > 0){
+console.log ("positivo")
+}else if (numero < 0){
+console.log("negativo")
+} else{
+    console.log("zero")
+}
---
+++ sabado1.js
@@ -0,0 +1,10 @@
+//crie um programa que verifica se um numero é par ou ímpar
+
+const numero = 13
+
+
+if(numero % 2 === 0){
+console.log("par")
+} else {
+    console.log("ímpar")
+}
